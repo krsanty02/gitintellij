ca5be4f40df15f10a9f7d23d645444a09fff2012
Santosh <krsanty02@gmail.com>
2026-09-12 00:40:58 +0530

    Adding B3 in class B

--- src/main/java/org/example/B.java
+++ src/main/java/org/example/B.java
@@ -3,6 +3,6 @@ package org.example;
 public class B {
     public static void main(String[] args) {
         System.out.println("B");
-        System.out.println("B1");
+        System.out.println("B3");
     }
 }
