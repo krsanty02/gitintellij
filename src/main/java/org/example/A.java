1c8ff65f3627628fa72fdedbac9e15c0e7566ab5
Santosh <krsanty02@gmail.com>
2026-09-12 22:37:12 +0530

    Adding z in  file A

--- src/main/java/org/example/A.java
+++ src/main/java/org/example/A.java
@@ -5,7 +5,7 @@ public class A {
         System.out.println("A");
         System.out.println("A1");
          System.out.println("B1");
-         System.out.println("c");
+         System.out.println("Z");
     }
 
 }
