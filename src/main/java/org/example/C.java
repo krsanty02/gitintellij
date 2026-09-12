065437c923b7feae046a174a2efdf2ebb89db38f
Santosh <krsanty02@gmail.com>
2026-09-13 00:23:34 +0530

    Adding hii456 in  file C master

--- src/main/java/org/example/C.java
+++ src/main/java/org/example/C.java
@@ -7,7 +7,7 @@ public class C {
         System.out.println("C2");
         System.out.println("C3");
         System.out.println("harami");
-        System.out.println("hii");
+        System.out.println("hii456");
     }
 
 }
