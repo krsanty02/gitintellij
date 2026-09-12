3e9fd785bd2997a3d2ba81ff4951be1e7e3586b9
Santosh <krsanty02@gmail.com>
2026-09-12 22:53:13 +0530

    Adding santoshsingh in  file A

--- src/main/java/org/example/A.java
+++ src/main/java/org/example/A.java
@@ -6,7 +6,7 @@ public class A {
         System.out.println("A1");
          System.out.println("B1");
          System.out.println("K");
-        System.out.println("santosh");
+        System.out.println("santoshsingh");
     }
 
 }
